e56a1d6483ceebf03f6e598d87ca7a9cf77cc8a3
Agnieszka <ar21@o2.pl>
2018-02-13 16:14:40 +0100

    add main slider

--- js/app.js
+++ js/app.js
@@ -1,6 +1,56 @@
-﻿var mobileViewport = window.matchMedia("screen and (max-width: 980px)");
+﻿// == hambuerger menu (X) ==
+function myFunction(x) {
+  x.classList.toggle("change");
+}
 
 $(document).ready(function(){
+
+  $('.slider').slick({
+
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 700,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4
+        }
+      },
+      {
+        breakpoint: 780,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+
+      {
+        breakpoint: 610,
+        settings: {
+          slidesToShow: 1
+        }
+        
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+
+
+  $('.main_slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows: false
+  });
+
+  /*
   $('.slider').slick({
     infinite: true,
     slidesToShow: 5,
@@ -9,4 +59,8 @@ $(document).ready(function(){
     autoplaySpeed: 700,
     arrows: false
   });
+  */
 });
+
+
+
